db0d39ef8052ea967e498904cdf228b18b754c84
Fares Al-din <faressaldinselim@gmail.com>
2026-03-06 14:44:08 +0200

    added welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,40 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HTML Test</title>
+    <style>
+        body {
+            font-family: 'Verdana', sans-serif;
+            background-color: #fffbe6;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 50px;
+        }
+        .content {
+            background: #fffbf0;
+            padding: 25px 35px;
+            border-radius: 15px;
+            box-shadow: 3px 3px 15px rgba(0,0,0,0.1);
+            max-width: 600px;
+        }
+        h1 {
+            font-size: 24px;
+            text-align: center;
+            color: #ff8c42;
+            margin-bottom: 15px;
+            text-shadow: 1px 1px 2px #ffd6a5;
+        }
+        p {
+            font-size: 16px;
+            line-height: 1.6;
+            color: #5c5c5c;
+        }
+    </style>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat consequat enim, at tincidunt lacus malesuada eget. Fusce nec magna lectus. Donec sit amet consequat est. Aliquam quis augue pharetra, ornare ligula sed, dignissim turpis. Interdum et malesuada fames ac ante ipsum ortor.</p>
+    <div class="content">
+        <h1>Welcome to My Page!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat consequat enim, at tincidunt lacus malesuada eget. Fusce nec magna lectus. Donec sit amet consequat est. Aliquam quis augue pharetra, ornare ligula sed, dignissim turpis. Interdum et malesuada fames ac ante ipsum ortor.</p>
+    </div>
 </body>
 </html>
